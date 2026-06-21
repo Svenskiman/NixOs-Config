500c79559f2de8ebfc5514b84603855cc7b670cc
Benjamin Miller <ben.hans.miller@gmail.com>
2026-06-21 21:33:19 +0100

    feat: clicking discord widget opens it

--- modules/home/media/discord.nix
+++ modules/home/media/discord.nix
@@ -1,9 +1,32 @@
 { lib, config, pkgs, ... }:
 
+let
+    discord-toggle = pkgs.writeShellApplication {
+        name = "discord-toggle";
+        runtimeInputs = [ pkgs.hyprland pkgs.jq ];
+        text = ''
+            window=$(hyprctl clients -j | jq -e '.[] | select(.class == "discord")' || true)
+
+            if [ -n "$window" ]; then
+                workspace=$(echo "$window" | jq -r '.workspace.id')
+                address=$(echo "$window" | jq -r '.address')
+                hyprctl dispatch "hl.dsp.focus({ workspace = $workspace })"
+                hyprctl dispatch "hl.dsp.focus({ window = { address = '$address' } })"
+            else
+                discord &
+                disown
+            fi
+        '';
+    };
+in
+
 {
     options.myModules.discord.enable = lib.mkEnableOption "Discord";
 
     config = lib.mkIf config.myModules.discord.enable {
-        home.packages = [ pkgs.discord ];
+        home.packages = [
+            pkgs.discord
+            discord-toggle
+        ];
     };
 }
