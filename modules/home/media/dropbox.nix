86a0958e34ae088a987a2e64cc1cdb95dc066d3c
Benjamin Miller <ben.hans.miller@gmail.com>
2026-06-21 21:55:11 +0100

    feat: clicking dropbox widget opens it in thunar

--- modules/home/media/dropbox.nix
+++ modules/home/media/dropbox.nix
@@ -1,9 +1,23 @@
 { lib, config, pkgs, ... }:
 
+# TODO: This is ass and needs adjusting
+# Should make a enum.nix or something defining applications like in bindings.nix
+let
+    dropbox-open = pkgs.writeShellApplication {
+        name = "dropbox-open";
+        runtimeInputs = [ pkgs.thunar ];
+        text = ''
+            thunar /home/svenski/.dropbox-hm/Dropbox/
+        '';
+    };
+in
+
 {
     options.myModules.dropbox.enable = lib.mkEnableOption "Dropbox sync";
 
     config = lib.mkIf config.myModules.dropbox.enable {
         services.dropbox.enable = true;
+
+        home.packages = [ dropbox-open ];
     };
 }
