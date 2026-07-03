6bc1d1657d69c120c5aa5b3e801721ac44e9921e
Svenski <ben.hans.miller@gmail.com>
2026-07-03 12:21:38 +0100

    feat: added dropdown system info

--- modules/home/scripts/eww/eww-dropdown.nix
+++ modules/home/scripts/eww/eww-dropdown.nix
@@ -12,10 +12,53 @@ let
             fi
         '';
     };
+
+    eww-cpu-status = pkgs.writeShellApplication {
+        name = "eww-cpu-status";
+        runtimeInputs = [ pkgs.gawk pkgs.jq pkgs.coreutils ];
+        text = ''
+            read -r _ u1 n1 s1 i1 _ < /proc/stat
+            sleep 0.5
+            read -r _ u2 n2 s2 i2 _ < /proc/stat
+            busy1=$((u1+n1+s1)); total1=$((u1+n1+s1+i1))
+            busy2=$((u2+n2+s2)); total2=$((u2+n2+s2+i2))
+            usage=$(awk -v b1="$busy1" -v t1="$total1" -v b2="$busy2" -v t2="$total2" \
+                'BEGIN { printf "%d", 100 * (b2-b1) / (t2-t1) }')
+            jq -nc --argjson usage "$usage" '{usage: $usage}'
+        '';
+    };
+
+    eww-ram-status = pkgs.writeShellApplication {
+        name = "eww-ram-status";
+        runtimeInputs = [ pkgs.gawk pkgs.jq ];
+        text = ''
+            usage=$(awk '/MemTotal/{t=$2} /MemAvailable/{a=$2} END{printf "%d", 100*(t-a)/t}' /proc/meminfo)
+            jq -nc --argjson usage "$usage" '{usage: $usage}'
+        '';
+    };
+
+    eww-gpu-status = pkgs.writeShellApplication {
+        name = "eww-gpu-status";
+        runtimeInputs = [ pkgs.jq ];
+        text = ''
+            FILE="/sys/class/drm/card0/device/gpu_busy_percent"
+            if [ -r "$FILE" ]; then
+                usage=$(cat "$FILE")
+                jq -nc --argjson usage "$usage" '{available: true, usage: $usage}'
+            else
+                jq -nc '{available: false, usage: 0}'
+            fi
+        '';
+    };
 in
 
 {
     config = lib.mkIf config.myModules.eww.enable {
-        home.packages = [ eww-dropdown-toggle ];
+        home.packages = [
+            eww-dropdown-toggle
+            eww-cpu-status
+            eww-ram-status
+            eww-gpu-status
+        ];
     };
 }
